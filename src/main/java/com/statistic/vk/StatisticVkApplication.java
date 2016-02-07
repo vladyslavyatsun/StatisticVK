f3f15ba7dd1a3246c89ab766a5c1c6ddc9da16ba
Ruslan Tsushko <ruslan-2009-ua@yandex.ru>
2016-02-07 16:32:08 +0200

    init commit

---
+++ src/main/java/com/statistic/vk/StatisticVkApplication.java
@@ -0,0 +1,12 @@
+package com.statistic.vk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticVkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticVkApplication.class, args);
+	}
+}
